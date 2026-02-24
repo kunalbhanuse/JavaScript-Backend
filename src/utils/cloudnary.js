4b9b16926fa49da481fd22901206178bd2a6ed1c
kunal bhanuse <kunalbhanuse07@gmail.com>
2026-02-24 17:19:57 +0530

    middelware multer to handle localpath

---
+++ src/utils/cloudnary.js
@@ -0,0 +1,25 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+// Configuration
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const uplodeOnCloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) return null;
+    // uplode the File on Cloudinary
+    const response = cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
+    // File is uploded  succesful
+    console.log("File is uploded on Cloudinary", response);
+    return response;
+  } catch (error) {
+    fs.unlinkSync(localFilePath); // remove the locally saved temporary file as uplode failed
+    return null;
+  }
+};
