2e4daab0033fbc51a0c3171757aff32d07211cb8
kunal bhanuse <kunalbhanuse07@gmail.com>
2026-02-26 01:16:28 +0530

    added subscription model

---
+++ src/models/subscription.model.js
@@ -0,0 +1,17 @@
+import mongoose, { Schema } from "mongoose";
+
+const subcriptionScema = new Schema(
+  {
+    subscriber: {
+      type: Schema.Types.ObjectId, // one who is subscribing
+      ref: "User",
+    },
+    channel: {
+      type: Schema.Types.ObjectId, // one to whom subscribing
+      ref: "User",
+    },
+  },
+  { timestamps: true },
+);
+
+export const Subscription = mongoose.model("Subscription", subcriptionScema);
